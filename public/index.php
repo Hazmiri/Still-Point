fc546b2e95ce7d25565b5f861e38356a0023b5d5
Saladin <hazmiri-sm@hotmail.com>
2026-04-15 18:45:46 +0100

    Create home page skeleton for Still Point.

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Load shared project setup.
+ */
+require_once __DIR__ . '/../src/bootstrap.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Still Point</title>
+</head>
+<body>
+
+<h1>Still Point</h1>
+
+<p>A curated collection of precision cue instruments.</p>
+
+</body>
+</html>
