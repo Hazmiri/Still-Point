0dd01c6960f4b1408d762be4db0e2838dde06030
Saladin <hazmiri-sm@hotmail.com>
2026-04-17 20:25:57 +0100

    Introduce semantic header and main landmarks.

--- templates/header.php
+++ templates/header.php
@@ -17,18 +17,22 @@ if (!isset($pageTitle) || $pageTitle === '') {
     <meta charset="UTF-8">
     <title><?= htmlspecialchars($pageTitle, ENT_QUOTES, 'UTF-8') ?></title>
 </head>
+
 <body>
+
 <header>
-<nav aria-label="Main site navigation">
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="collection.php">Collection</a></li>
-        <li><a href="provenance.php">Provenance</a></li>
-        <li><a href="contact.php">Contact</a></li>
-        <li><a href="faq.php">FAQ</a></li>
-        <li><a href="login.php">Custodian Access</a></li>
-    </ul>
-</nav>
+
+    <nav aria-label="Main site navigation">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="collection.php">Collection</a></li>
+            <li><a href="provenance.php">Provenance</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="faq.php">FAQ</a></li>
+            <li><a href="login.php">Custodian Access</a></li>
+            </ul>
+    </nav>
+
 </header>
 
 <main>
